6c6cd6cf51de1eb9f1a04242e7005617d2a2874b
morten <mmat@eaaa.dk>
2015-03-16 15:43:35 +0100

    Migrate project to AndroidStudio

--- GlassCow/src/com/eaaa/glasscow/service/DatabaseFields.java
+++
@@ -1,9 +0,0 @@
-package com.eaaa.glasscow.service;
-
-public class DatabaseFields
-{
-	public static final String TABLE_COW = "cow";
-	
-	public static final String FIELD_ID = "id";
-	public static final String FIELD_JSON = "json";
-}
