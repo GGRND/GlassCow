e36c9ebbf2168d44e151970bd2cc77fb207aa1e6
Nicklas Nielsen <husblas@gmail.com>
2016-07-28 10:49:57 +0200

    no message

--- app/src/main/java/com/eaaa/glasscow/transfer_cows/Activity_Transfer_Cow.java
+++ app/src/main/java/com/eaaa/glasscow/transfer_cows/Activity_Transfer_Cow.java
@@ -1,5 +1,6 @@
 package com.eaaa.glasscow.transfer_cows;
 
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
@@ -16,30 +17,23 @@ import com.eaaa.glasscow.MenuHandler;
 import com.eaaa.glasscow.R;
 import com.eaaa.glasscow.model.Cow;
 import com.eaaa.glasscow.service.RemoteDatabase;
-import com.eaaa.glasscow.transfer_cows.Transfer_Cow;
 import com.google.android.glass.view.WindowUtils;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.List;
 
 /**
  * Created by ThinkNick on 22-07-2016.
  */
-public class Activity_Transfer_Cow extends Transfer_Cow {
+public class Activity_Transfer_Cow extends Transfer_Interface {
     private String newHerdID = "";
 
     private TextView cowIDView, herdIDView, nherdIDView, dateTextView, firstDescription, secondDescription;
     private RelativeLayout transferView, dateView;
-    private Menu menu;
 
     private Activity_Main ctx;
     private RemoteDatabase remoteDatabase;
-    private Cow cow;
+
 
     private final long transferCodeId = 19;
-    private String animalNumber;
-    private String date, herdId, shortAnimalNumber;
+
     private int menuNumberCounter = 1;
 
     // Menu item ids:
@@ -74,16 +68,6 @@ public class Activity_Transfer_Cow extends Transfer_Cow {
         setNewherdID();
     }
 
-    /**
-     * Gets the required information about the current cow
-     */
-    public void getCowInfo() {
-        cow = com.eaaa.glasscow.Activity_Main.cow;
-        herdId = cow.getHerdId();
-        shortAnimalNumber = cow.getShortNumber();
-        animalNumber = cow.getAnimalId();
-    }
-
 
     public void initElements() {
         cowIDView = (TextView) findViewById(R.id.CowID_transfer);
@@ -91,15 +75,16 @@ public class Activity_Transfer_Cow extends Transfer_Cow {
         nherdIDView = (TextView) findViewById(R.id.NHerdID_transfer);
 
         dateTextView = (TextView) findViewById(R.id.date_transfer);
-        //firstDescription = (TextView) findViewById(R.id.first_description_transfer);
-        //secondDescription = (TextView) findViewById(R.id.second_description_transfer);
+        firstDescription = (TextView) findViewById(R.id.first_description_transfer);
+        secondDescription = (TextView) findViewById(R.id.second_description_transfer);
         transferView = (RelativeLayout) findViewById(R.id.herd_transfer);
         dateView = (RelativeLayout) findViewById(R.id.date_text_transfer);
     }
 
     public void setElements() {
-        cowIDView.setText(removeZero(shortAnimalNumber));
-        herdIDView.setText(removeZero(herdId));
+        cowIDView.setText(removeZero(super.getShortAnimalNumber()));
+        herdIDView.setText(removeZero(super.getHerdId()));
+        dateView = (RelativeLayout) findViewById(R.id.date_text_transfer);
 
     }
 
@@ -112,40 +97,41 @@ public class Activity_Transfer_Cow extends Transfer_Cow {
      * Toggles visibility of certain views given the context of the field variable menuNumberCounter
      */
     public void setCertainViewVisible() {
-        if (menuNumberCounter == 1) {
-            //firstDescription.setVisibility(View.INVISIBLE);
-            transferView.setVisibility(View.VISIBLE);
-            //secondDescription.setVisibility(View.VISIBLE);
-        }
         if (menuNumberCounter == 2) {
+            secondDescription.setVisibility(View.VISIBLE);
+        }
+        if (menuNumberCounter > 65) {
+            transferView.setVisibility(View.VISIBLE);
 
         }
     }
     public void createNumbersInSubMenu(Menu menu) {
-                SubMenu subMenu0 = menu.addSubMenu(Menu.NONE, MENU0, Menu.NONE, "0");
+        if (menuNumberCounter == 1 && newHerdID.length() <= 6) {
+            SubMenu subMenu0 = menu.addSubMenu(Menu.NONE, MENU0, Menu.NONE, "0");
 
-                SubMenu subMenu1 = menu.addSubMenu(Menu.NONE, MENU1, Menu.NONE, "1");
-
-
-                SubMenu subMenu2 = menu.addSubMenu(Menu.NONE, MENU2, Menu.NONE, "2");
+            SubMenu subMenu1 = menu.addSubMenu(Menu.NONE, MENU1, Menu.NONE, "1");
 
 
-                SubMenu subMenu3 = menu.addSubMenu(Menu.NONE, MENU3, Menu.NONE, "3");
+            SubMenu subMenu2 = menu.addSubMenu(Menu.NONE, MENU2, Menu.NONE, "2");
 
 
-                SubMenu subMenu4 = menu.addSubMenu(Menu.NONE, MENU4, Menu.NONE, "4");
+            SubMenu subMenu3 = menu.addSubMenu(Menu.NONE, MENU3, Menu.NONE, "3");
 
 
-                SubMenu subMenu5 = menu.addSubMenu(Menu.NONE, MENU5, Menu.NONE, "5");
+            SubMenu subMenu4 = menu.addSubMenu(Menu.NONE, MENU4, Menu.NONE, "4");
 
-                SubMenu subMenu6 = menu.addSubMenu(Menu.NONE, MENU6, Menu.NONE, "6");
 
-                SubMenu subMenu7 = menu.addSubMenu(Menu.NONE, MENU7, Menu.NONE, "7");
+            SubMenu subMenu5 = menu.addSubMenu(Menu.NONE, MENU5, Menu.NONE, "5");
 
-                SubMenu subMenu8 = menu.addSubMenu(Menu.NONE, MENU8, Menu.NONE, "8");
+            SubMenu subMenu6 = menu.addSubMenu(Menu.NONE, MENU6, Menu.NONE, "6");
 
-                SubMenu subMenu9 = menu.addSubMenu(Menu.NONE, MENU9, Menu.NONE, "9");
-            }
+            SubMenu subMenu7 = menu.addSubMenu(Menu.NONE, MENU7, Menu.NONE, "7");
+
+            SubMenu subMenu8 = menu.addSubMenu(Menu.NONE, MENU8, Menu.NONE, "8");
+
+            SubMenu subMenu9 = menu.addSubMenu(Menu.NONE, MENU9, Menu.NONE, "9");
+        }
+    }
 
 
 
@@ -159,13 +145,14 @@ public class Activity_Transfer_Cow extends Transfer_Cow {
                 if (menuNumberCounter == 1) {
                     dateView.setVisibility(View.VISIBLE);
                     setCurrentDate(dateTextView, dateView);
-                    createNumbersInSubMenu(menu);
+                    createNumbersInSubMenu(super.getMenu());
+                    firstDescription.setVisibility(View.INVISIBLE);
 
                 }
-                if (menuNumberCounter == 2) {
+                if (menuNumberCounter == 3) {
                     //remoteDatabase.send2Herd(herdId, newHerdID, animalNumber, transferCodeId, date);
+                    Log.d("BEKRAEFTSEND", "yes");
                 }
-                menuNumberCounter++;
 
                 break;
 
@@ -173,95 +160,99 @@ public class Activity_Transfer_Cow extends Transfer_Cow {
                 if (menuNumberCounter == 1) {
 
                 }
-                if (menuNumberCounter == 2) {
+                if (menuNumberCounter == 3) {
+                    newHerdID = "";
+                    setNewherdID();
+                    menuNumberCounter = 1;
+                    recreate();
 
                 }
                 //TODO manuelt indtale dato.
                 break;
 
         }
-        if (newHerdID.length() < 7) {
+if(menuNumberCounter == 1 && newHerdID.length() <= 6) {
+    switch (item.getItemId()) {
 
-            switch (item.getItemId()) {
+        case MENU0:
+            newHerdID += 0;
+            createNumbersInSubMenu(super.getMenu());
+            setNewherdID();
+            break;
 
-                case MENU0:
-                    newHerdID += 0;
-                    createNumbersInSubMenu(menu);
-                    setNewherdID();
-                    break;
+        case MENU1:
+            newHerdID += 1;
+            createNumbersInSubMenu(super.getMenu());
+            setNewherdID();
+            break;
 
-                case MENU1:
-                    newHerdID += 1;
-                    createNumbersInSubMenu(menu);
-                    setNewherdID();
-                    break;
+        case MENU2:
+            newHerdID += 2;
+            createNumbersInSubMenu(super.getMenu());
+            setNewherdID();
+            break;
 
-                case MENU2:
-                    newHerdID += 2;
-                    createNumbersInSubMenu(menu);
-                    setNewherdID();
-                    break;
+        case MENU3:
+            newHerdID += 3;
+            createNumbersInSubMenu(super.getMenu());
+            setNewherdID();
 
-                case MENU3:
-                    newHerdID += 3;
-                    createNumbersInSubMenu(menu);
-                    setNewherdID();
+            break;
 
-                    break;
+        case MENU4:
+            newHerdID += 4;
+            createNumbersInSubMenu(super.getMenu());
+            setNewherdID();
 
-                case MENU4:
-                    newHerdID += 4;
-                    createNumbersInSubMenu(menu);
-                    setNewherdID();
+            break;
 
-                    break;
+        case MENU5:
+            newHerdID += 5;
+            createNumbersInSubMenu(super.getMenu());
+            setNewherdID();
 
-                case MENU5:
-                    newHerdID += 5;
-                    createNumbersInSubMenu(menu);
-                    setNewherdID();
+            break;
 
-                    break;
+        case MENU6:
+            newHerdID += 6;
+            createNumbersInSubMenu(super.getMenu());
+            setNewherdID();
 
-                case MENU6:
-                    newHerdID += 6;
-                    createNumbersInSubMenu(menu);
-                    setNewherdID();
+            break;
 
-                    break;
+        case MENU7:
+            newHerdID += 7;
+            createNumbersInSubMenu(super.getMenu());
+            setNewherdID();
 
-                case MENU7:
-                    newHerdID += 7;
-                    createNumbersInSubMenu(menu);
-                    setNewherdID();
+            break;
 
-                    break;
+        case MENU8:
+            newHerdID += 8;
+            createNumbersInSubMenu(super.getMenu());
+            setNewherdID();
 
-                case MENU8:
-                    newHerdID += 8;
-                    createNumbersInSubMenu(menu);
-                    setNewherdID();
+            break;
 
-                    break;
+        case MENU9:
+            newHerdID += 9;
+            createNumbersInSubMenu(super.getMenu());
+            setNewherdID();
 
-                case MENU9:
-                    newHerdID += 9;
-                    createNumbersInSubMenu(menu);
-                    setNewherdID();
+            break;
 
-                    break;
-
-                // }
-            }
+        // }
+    }
+}
 Log.d("Mnunumber", String.valueOf(menuNumberCounter));
 
-            if (newHerdID.length() >= 6) {
-                setCertainViewVisible();
+            if (newHerdID.length() >= 7) {
                 menuNumberCounter++;
-
-
+                setCertainViewVisible();
+                MenuHandler.yesNoMenuItems(getMenu());
+                menuNumberCounter++;
             }
-        }
+
         return super.onMenuItemSelected(featureId, item);
     }
 
@@ -272,6 +263,7 @@ Log.d("Mnunumber", String.valueOf(menuNumberCounter));
             MenuHandler.yesNoMenuItems(menu);
             return true;
         }
+
         return super.onCreatePanelMenu(featureId, menu);
     }
 
